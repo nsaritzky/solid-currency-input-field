415ced4e16fab26327d010d30d9dafbf6b518e13
nsaritzky <nsaritzky@gmail.com>
2023-10-02 10:37:42 -0700

    Added index.tsx (doh!)

---
+++ src/index.tsx
@@ -0,0 +1,31 @@
+import { type Accessor, type Component, createComputed, createSignal } from 'solid-js'
+import CurrencyInput from './components/CurrencyInput'
+
+export type { CurrencyInputProps } from './components/CurrencyInputProps'
+export default CurrencyInput
+export { formatValue } from './components/utils/formatValue'
+
+export function createHello(): [Accessor<string>, (to: string) => void] {
+  const [hello, setHello] = createSignal('Hello World!')
+
+  return [hello, (to: string) => setHello(`Hello ${to}!`)]
+}
+
+export const Hello: Component<{ to?: string }> = props => {
+  const [hello, setHello] = createHello()
+
+  // Console calls will be removed in production if `dropConsole` is enabled
+
+  // eslint-disable-next-line no-console
+  console.log('Hello World!')
+
+  createComputed(() => {
+    if (typeof props.to === 'string') setHello(props.to)
+  })
+
+  return (
+    <>
+      <div>{hello()}</div>
+    </>
+  )
+}
